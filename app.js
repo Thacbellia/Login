c19b0fe59b7599a11cf9701a84d588974bbf9c43
Thacbellia <belliathac@gmail.com>
2026-04-17 11:29:17 +0200

    index page is chaged

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
 
-const supabaseUrl = "YOUR_PROJECT_URL"
+const supabaseUrl = "https://xfsctjluyctwoygzgzew.supabase.co"
 const supabaseKey = "YOUR_ANON_KEY"
 
 const supabase = createClient(supabaseUrl, supabaseKey)
