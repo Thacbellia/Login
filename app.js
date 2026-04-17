452383229beb89189ce3824eb1fa14a985c49758
Thacbellia <belliathac@gmail.com>
2026-04-17 12:56:30 +0200

    js code is updated

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ const supabaseUrl = "https://xfsctjluyctwoygzgzew.supabase.co"
 const supabaseKey = "sb_publishable_1UwCJUStuhslAXgIgNw3rA_nB7XTP7t"
 
 const supabase = createClient(supabaseUrl, supabaseKey)
+useEffect(() => {
+  console.log(process.env.NEXT_PUBLIC_SUPABASE_URL)
+}, [])
