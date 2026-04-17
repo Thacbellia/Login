1d60d4c0a972f03260e2b586f1cc9cfa0b250b23
Thacbellia <belliathac@gmail.com>
2026-04-17 11:37:49 +0200

    js is updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
 
 const supabaseUrl = "https://xfsctjluyctwoygzgzew.supabase.co"
-const supabaseKey = "YOUR_ANON_KEY"
+const supabaseKey = "sb_publishable_1UwCJUStuhslAXgIgNw3rA_nB7XTP7t"
 
 const supabase = createClient(supabaseUrl, supabaseKey)
